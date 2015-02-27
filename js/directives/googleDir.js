c1b7d1876adeb3a1e7aaa9a69f2052b8202051ac
pankaj-arunsingh <pankaj_arunsingh@ymail.com>
2015-02-27 14:40:02 +0530

    Update and rename js/directives to js/directives/googleDir.js

---
+++ js/directives/googleDir.js
@@ -0,0 +1,57 @@
+'use strict';  
+
+		App.directive('googlePlaces', function(){
+                return {
+                    restrict:'E',
+                    replace:true,
+                    // transclude:true,
+                    scope: {location:'='},
+		    
+		    
+                    template: '<input id="google_places_ac" name="google_places_ac" type="text" class="input-block-level"/>',
+		    
+                    link: function($scope, elm, attrs){
+                        var autocomplete = new google.maps.places.Autocomplete($("#google_places_ac")[0], {});
+                        google.maps.event.addListener(autocomplete, 'place_changed', function() {
+                            var place = autocomplete.getPlace();
+                            $scope.location = place.geometry.location.lat() + ',' + place.geometry.location.lng();
+			
+                            $scope.$apply();
+			
+
+
+			var mapOptions = {
+        zoom: 12,
+        center: new google.maps.LatLng(20.0000, 68.0000),
+        mapTypeId: google.maps.MapTypeId.TERRAIN
+    }
+var latlon =[
+{
+lat:place.geometry.location.lat(),
+lon:place.geometry.location.lng()
+
+}];
+    $scope.map = new google.maps.Map(document.getElementById('map'), mapOptions);
+
+    $scope.markers = [];
+  var createMarker = function (info){
+        
+        var marker = new google.maps.Marker({
+            map: $scope.map,
+            position: new google.maps.LatLng(place.geometry.location.lat(), place.geometry.location.lng())
+           
+        });
+      
+        
+       
+        
+
+        
+    }  
+    
+    createMarker(latlon[0]);
+
+                        });
+                    }
+                }
+            });
