964a2b8b65097bfa5e0078c8fc9b09039e916063
vennila21 <vennila.92@gmail.com>
2015-03-04 12:21:09 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 "use strict";
 
-var App = angular.module("todo", ["ui.sortable", "LocalStorageModule"]);
+var App = angular.module("todo", ["LocalStorageModule"]);
 App.controller("TodoCtrl", function ($scope, localStorageService) {
 	$scope.init = function () {
 		if (!localStorageService.get("todoList")) {
@@ -27,6 +27,7 @@ App.controller("TodoCtrl", function ($scope, localStorageService) {
 
 	$scope.addTodo = function  () {
 		/*Should prepend to array*/
+alert("todo");
 		$scope.model[$scope.currentShow].list.splice(0, 0, {taskName: $scope.newTodo, isDone: false });
 		/*Reset the Field*/
 		$scope.newTodo = "";
@@ -41,6 +42,8 @@ App.controller("TodoCtrl", function ($scope, localStorageService) {
 		cursor: "move",
 		tolerance: "pointer"
 	};
+
+	
 
 	/* Filter Function for All | Incomplete | Complete */
 	$scope.showFn = function  (todo) {
@@ -78,5 +81,95 @@ App.controller('menuBar', function($scope) {
     $scope.myVar = !$scope.myVar;
     };
 });
+
+App.controller("loginCtrl",function($scope){
+$scope.login=true;
+$scope.register=false;
+var userRecord= JSON.parse(localStorage.userData || "null") || [];
+$scope.loginClick=function(){
+var flag=0;
+var userName=document.getElementById("uname").value;
+var password=document.getElementById("password").value;
+localStorage.usrname=userName;
+
+if ($("#rememberMe").is(' :checked' )){
+//save username and password
+localStorage.usrname=userName;
+localStorage.pass=password
+localStorage.chkbx=$("#rememberMe").val();
+}
+else{
+localStorage.usrname="";
+localStorage.pass="";
+localStorage.chkbx="";
+}
+for (var j = 0; j < userRecord.length; j++){
+  if((userRecord[j].userName==userName)&&(userRecord[j].password==password))
+{
+flag=1;
+localStorage.fullName=userRecord[j].fisrtName+" "+userRecord[j].lastName;
+break;
+}
+}
+if(flag==1)
+{
+
+window.location="index.html";
+return true;
+}
+else{
+alert("You are not a registered user.\n Please get registered by clicking on New user Button !");
+return false;
+}
+};
+});
+
+App.controller("registerCtrl",function($scope){
+
+$scope.registerClick=function(){
+$scope.login=false;
+$scope.register=true;
+
+  var userData = loadData();
+
+        // Wait for user to add new ones
+      
+      $scope.registerMe=function() {
+  emailAddress = document.getElementById("email").value,
+  userId = document.getElementById("userName").value,
+ pass = document.getElementById("pwd").value,
+            data = {
+             email: emailAddress,
+             userName: userId,
+             password: pass,		 	
+              id:   +new Date()
+            };
+        if (userName) {
+          userData.push(data);
+          saveData();
+          
+        }
+
+ // Get the responses (if any) or an empty array
+      function loadData() {
+        return JSON.parse(localStorage.userData || "null") || [];
+
+      }
+//Save the new data in local storage
+      function saveData() {
+        localStorage.userData= JSON.stringify(userData);
+      }
+
+};
+
+};
+});
+
+
+
+
+
+
+
 
 
